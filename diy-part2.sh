d48298688ee4c05fc0816379fa12c466c4393f8c
Pierluigi97 <barsanofiopierluigimoretto@gmail.com>
2026-07-09 21:12:43 +0200

    Modify default settings in diy-part2.sh
    Updated default IP, hostname, and theme in OpenWrt script.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+# OpenWrt DIY script part 2 (After Update feeds)
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. Modifica l'IP di default del router per evitare conflitti con il modem 5G
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 2. Modifica il nome del router (Hostname)
+sed -i 's/OpenWrt/BPI-R4-5G/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 3. Imposta il tema grafico Argon come predefinito all'avvio
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
