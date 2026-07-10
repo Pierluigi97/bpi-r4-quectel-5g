fc86a8d399f54e3e204a62dbe2359ee6dc4c4261
Pierluigi97 <barsanofiopierluigimoretto@gmail.com>
2026-07-10 10:36:55 +0200

    Update router IP, hostname, and default theme

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#
 # OpenWrt DIY script part 2 (After Update feeds)
+#
 
 # 1. Modifica l'IP di default del router per evitare conflitti con il modem 5G
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
