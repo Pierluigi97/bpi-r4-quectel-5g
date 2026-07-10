4e85bdff933e2fbfc1f15d6b1882cd4a3b4608b3
Pierluigi97 <barsanofiopierluigimoretto@gmail.com>
2026-07-10 10:36:19 +0200

    Add comments and update Argon theme feed

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#
 # OpenWrt DIY script part 1 (Before Update feeds)
+#
 
 # Aggiunge il feed di Siriling per i driver 5G PCIe del Quectel
 echo 'src-git siriling5g https://github.com/Siriling/5G-Modem-Support.git' >> feeds.conf.default
